371a7ce7dc7be0696b675849aadb3177e65b3d54
katelinmaguire <47308450+katelinmaguire@users.noreply.github.com>
2020-06-16 20:51:24 -0400

    updatet

--- Dev-KM/Map/static/js/logic.js
+++ Dev-KM/Map/static/js/logic.js
@@ -1,31 +1,5 @@
 // .geojson files: https://github.com/dwillis/nyc-maps
 
-// create map object
-var myMap = L.map("map", {
-  center: [40.71, -74.01],
-  zoom: 11
-});
-
-// Adding tile layer
-L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-  attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
-  tileSize: 512,
-  maxZoom: 18,
-  zoomOffset: -1,
-  id: "mapbox/streets-v11",
-  accessToken: API_KEY
-}).addTo(myMap);
-
-  // add tile layer
-  L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-      attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-      maxZoom: 18,
-      id: 'mapbox/streets-v11',
-      tileSize: 512,
-      zoomOffset: -1,
-      accessToken: API_KEY
-  }).addTo(myMap);
-
 // Load in geojson data
 var geoData = "static/data/nyc_school_districts.geojson";
 
@@ -38,7 +12,7 @@ d3.json(geoData, function(data) {
 d3.json(geoData, function(data) {
 
   // Create a new choropleth layer
-  var geojson = L.choropleth(data, {
+  var geojson1 = L.choropleth(data, {
 
     // Define what  property in the features to use
     valueProperty: "poverty",
@@ -67,118 +41,129 @@ d3.json(geoData, function(data) {
   });
 
   // Add the choropleth layer to the map
-  geojson.addTo(myMap);
+  //geojson1.addTo(myMap);
 
-  // Set up the legend
-  var legend = L.control({ position: "bottomleft" });
-
-  // onAdd: "the map calls the onAdd() method of the layer, then the layer
-  //        creates its HTML element(s) (commonly named ‘container’ element)
-  //        and adds them to the map pane."
-  // Ref: https://leafletjs.com/examples/extending/extending-2-layers.html
-  legend.onAdd = function() {
-    var div = L.DomUtil.create("div", "info legend");
-    var limits = geojson.options.limits;
-    var colors = geojson.options.colors;
-    var labels = [];
-
-    // Add min & max
-    var legendInfo = "<h1>Poverty Rate</h1>" +
-      "<div class=\"labels\">" +
-        "<div class=\"min\">" + limits[0] + "</div>" +
-        "<div class=\"max\">" + limits[limits.length - 1] + "</div>" +
-      "</div>";
-
-    div.innerHTML = legendInfo;
-
-    limits.forEach(function(limit, index) {
-      labels.push("<li style=\"background-color: " + colors[index] + "\"></li>");
-    });
-
-    div.innerHTML += "<ul>" + labels.join("") + "</ul>";
-    return div;
-  };
-
-  // Adding legend to the map
-  legend.addTo(myMap);
+  createMap(geojson1);
 
 });
 
-// By borough
-d3.json(geoData2, function(data) {
-  console.log(data);
-});
 
-
-// TO DO - Add mean test score to map as new layer 
 var geoData2 = "static/data/boroughs.geojson";
 
-// d3.json(geoData2, function(data) {
+d3.json(geoData2, function(data) {
 
-//   // Create a new choropleth layer
-//   var geojson = L.choropleth(data, {
+  // Create a new choropleth layer
+  var geojson2 = L.choropleth(data, {
 
-//     // Define what  property in the features to use
-//     valueProperty: "Shape_Area",
+    // Define what  property in the features to use
+    valueProperty: "Shape_Area",
 
-//     // Set color scale (in hex values)
-//     scale: ["#ffffb2", "#b10026"],
+    // Set color scale (in hex values)
+    scale: ["#ffffb2", "#b10026"],
 
-//     // Number of breaks in step range
-//     steps: 10,
+    // Number of breaks in step range
+    steps: 10,
 
-//     // q for quantile, e for equidistant, k for k-means
-//     mode: "q",
-//     style: {
-//       // Border color
-//       color: "#fff",
-//       weight: 1,
-//       fillOpacity: 0.8
-//     },
+    // q for quantile, e for equidistant, k for k-means
+    mode: "q",
+    style: {
+      // Border color
+      color: "#fff",
+      weight: 1,
+      fillOpacity: 0.8
+    },
 
-//     // Binding a pop-up to each layer
-//     onEachFeature: function(feature, layer) {
-//       layer.bindPopup("Borough: " + feature.properties.BoroName +
-//                       "<br>Test Score:<br>" +
-//                       feature.properties.Shape_Area + "%");   // this value is a placeholder
-//     }
-//   });
+    // Binding a pop-up to each layer
+    onEachFeature: function(feature, layer) {
+      layer.bindPopup("Borough: " + feature.properties.BoroName +
+                      "<br>Test Score:<br>" +
+                      feature.properties.Shape_Area + "%");   // this value is a placeholder
+    }
+  });
 
-//   // Add the choropleth layer to the map
-//   geojson.addTo(myMap);
+  createMap(geojson2);
 
-//   // Set up the legend
-//   var legend = L.control({ position: "bottomleft" });
+ });
 
-//   // onAdd: "the map calls the onAdd() method of the layer, then the layer
-//   //        creates its HTML element(s) (commonly named ‘container’ element)
-//   //        and adds them to the map pane."
-//   // Ref: https://leafletjs.com/examples/extending/extending-2-layers.html
-//   legend.onAdd = function() {
-//     var div = L.DomUtil.create("div", "info legend");
-//     var limits = geojson.options.limits;
-//     var colors = geojson.options.colors;
-//     var labels = [];
+function createMap(geojson2) {
+  
+  console.log(geojson2);
 
-//     // Add min & max
-//     var legendInfo = "<h1>ELA State Test Score</h1>" +
-//       "<div class=\"labels\">" +
-//         "<div class=\"min\">" + limits[0] + "</div>" +
-//         "<div class=\"max\">" + limits[limits.length - 1] + "</div>" +
-//       "</div>";
+  // Adding tile layer
+  var streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+      attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+      tileSize: 512,
+      maxZoom: 18,
+      zoomOffset: -1,
+      id: "mapbox/streets-v11",
+      accessToken: API_KEY
+  })
 
-//     div.innerHTML = legendInfo;
+  // add tile layer
+  var lightmap =L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+      attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+      maxZoom: 18,
+      id: 'mapbox/light-v10',
+      tileSize: 512,
+      zoomOffset: -1,
+      accessToken: API_KEY
+  })
 
-//     limits.forEach(function(limit, index) {
-//       labels.push("<li style=\"background-color: " + colors[index] + "\"></li>");
-//     });
+  // base layers
+  var baseMaps = {
+    "Street Map": streetmap,
+    "Light Map": lightmap
+  };
 
-//     div.innerHTML += "<ul>" + labels.join("") + "</ul>";
-//     return div;
-//   };
+    // overlay layer
+  var overlayMaps = {
+    Boroughs: geojson2
+    //Districts: geojson2
+  };
 
-//   // Adding legend to the map
-//   legend.addTo(myMap);
+    // create map object
+  var myMap = L.map("map", {
+      center: [40.71, -74.01],
+      zoom: 11,
+      layers: [streetmap, geojson2]  // to display when page load
+  });
+  
+    // layer control with baseMaps and overlayMaps
+    L.control.layers(baseMaps, overlayMaps, {
+      collapsed: false
+    }).addTo(myMap);  // add these to the map
 
-// });
+
+    // Set up the legend 2
+    var legend2 = L.control({ position: "bottomleft" });
+  
+    // legend 2
+    legend2.onAdd = function() {
+      var div = L.DomUtil.create("div", "info legend");
+      var limits = geojson2.options.limits;
+      var colors = geojson2.options.colors;
+      var labels = [];
+  
+      // Add min & max
+      var legendInfo = "<h1>ELA State Test Score</h1>" +
+        "<div class=\"labels\">" +
+          "<div class=\"min\">" + limits[0] + "</div>" +
+          "<div class=\"max\">" + limits[limits.length - 1] + "</div>" +
+        "</div>";
+  
+      div.innerHTML = legendInfo;
+  
+      limits.forEach(function(limit, index) {
+        labels.push("<li style=\"background-color: " + colors[index] + "\"></li>");
+      });
+  
+      div.innerHTML += "<ul>" + labels.join("") + "</ul>";
+      return div;
+    };
+  
+    // Adding legend to the map
+    legend2.addTo(myMap);
+  
+}
+
 
